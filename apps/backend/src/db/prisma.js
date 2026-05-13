65d798073c452f6ecec53cc36f432a24bc97cf80
PHCGV <PHCGV@hotmail.com>
2026-05-13 15:46:46 -0300

    Reapply "prisma ta complicado"
    This reverts commit aa370b62dab4c32d8bce0a7ca2babb87798ded6a.

--- apps/backend/src/db/prisma.js
+++ apps/backend/src/db/prisma.js
@@ -1,9 +1,9 @@
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "../config/env.js";
 
 const adapter = new PrismaPg({
-	connectionString: config.databaseUrl
+  connectionString: config.databaseUrl
 });
 
 export const prisma = new PrismaClient({ adapter });
